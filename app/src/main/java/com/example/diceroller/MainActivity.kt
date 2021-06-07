183693865a4565ce4be30d8f8c2f2c99f9dd5d8d
Fujita Akiko <yanmi3970@gmail.com>
2021-06-07 11:03:51 +0900

    サイコロの目に合わせた画像表示の実装

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -24,15 +24,32 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         // 6面のサイコロのインスタンスを作成
         val dice = Dice(6)
+        // サイコロの目を取得する
+        var diceRoll = dice.roll()
         // 結果を表示するTextViewを探す
         val resultTextView: TextView = findViewById(R.id.resultText)
         // サイコロの目をstring型に変換し、TextViewの更新を行う
-        resultTextView.text = dice.roll().toString()
+        resultTextView.text = diceRoll.toString()
+
         // サイコロの結果に合わせて画像を表示する
         val diceImage: ImageView = findViewById(R.id.diceImage)
-        diceImage.setImageResource(R.drawable.dice_2)
+
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
+
     }
 }
+
 /*
  * 面の数を受け取って、サイコロを振るクラス
  */
